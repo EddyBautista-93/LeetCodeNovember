5c93146ba66fea788a232ce2892651b4ba75bac2
EddyBautista <bautista.j.eddy@gmail.com>
2020-11-30 20:52:12 -0600

    Array Dif Kata code wars

--- Program.cs
+++ Program.cs
@@ -90,32 +90,46 @@ namespace LeetCodeNovember
 
         // code exp : Set the return conditions as vars and using linq to get the list in asc and 
         // dsc order, if the sequence matches return the correct string
-        public static string IsSortedAndHow(int[] array)
+        //public static string IsSortedAndHow(int[] array)
+        //{
+        //    string yesAsc = "yes, ascending";
+        //    string yesDsc = "yes, descending";
+        //    string otherwise = "no";
+        //
+        //    var orderByAsc = array.OrderBy(a => a);
+        //    var orderByDsc = array.OrderByDescending(a => a);
+        //
+        //    if (array.SequenceEqual(orderByAsc))
+        //    {
+        //        return yesAsc;
+        //    }
+        //    else if (array.SequenceEqual(orderByDsc))
+        //    {
+        //        return yesDsc;
+        //    }
+        //    else
+        //    {
+        //        return otherwise;
+        //    }
+        //    // shorter way to write it
+        // if (array.OrderBy(a => a).SequenceEqual(array)) return "yes, ascending";
+        // if (array.OrderByDescending(a => a).SequenceEqual(array)) return "yes, descending";
+        // return "no";
+
+        //}
+        //Array.diff
+
+        //your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+        //It should remove all values from list a, which are present in list b.
+        //
+        //Kata.ArrayDiff(new int[] {1, 2}, new int[] {1}) => new int[] {2}
+        //If a value is present in b, all of its occurrences must be removed from the other:
+        //
+        //Kata.ArrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2}) => new int[] {1, 3}
+        public static int[] ArrayDiff(int[] a, int[] b)
         {
-            string yesAsc = "yes, ascending";
-            string yesDsc = "yes, descending";
-            string otherwise = "no";
-
-            var orderByAsc = array.OrderBy(a => a);
-            var orderByDsc = array.OrderByDescending(a => a);
-
-            if (array.SequenceEqual(orderByAsc))
-            {
-                return yesAsc;
-            }
-            else if (array.SequenceEqual(orderByDsc))
-            {
-                return yesDsc;
-            }
-            else
-            {
-                return otherwise;
-            }
-            // shorter way to write it
-           // if (array.OrderBy(a => a).SequenceEqual(array)) return "yes, ascending";
-           // if (array.OrderByDescending(a => a).SequenceEqual(array)) return "yes, descending";
-           // return "no";
-
+            return a.Where(n => !b.Contains(n)).ToArray();
         }
 
         private static void Main(string[] args)
